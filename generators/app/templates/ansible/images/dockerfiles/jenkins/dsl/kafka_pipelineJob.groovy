0fb3e4127467f28630f7410ef9b2456bd8e23b94
Chabane REFES <chabane.refes@gmail.com>
2017-02-19 20:17:43 +0100

    add pipeline job spring5 et kafka

--- generators/app/templates/ansible/images/dockerfiles/jenkins/dsl/kafka_pipelineJob.groovy
+++ generators/app/templates/ansible/images/dockerfiles/jenkins/dsl/kafka_pipelineJob.groovy
@@ -5,10 +5,17 @@ pipelineJob("kafka-pipeline") {
             script("""
                 node {
                       stage "checkout"
-
+                       git url : 'https://github.com/NirbyApp/mitosis-microservice-spring-kafka-reactor.git'
+                           
                       stage "test"
+                       sh './gradlew test'
+                      
+                      stage "build"
+                       sh './gradlew build'
 
                       stage "deploy"
+                       sh 'docker build -t mitosis/microservice-spring-kafka-reactor:1.0.0-alpha.0 .'
+                       sh 'docker run -d -p 9992:8080 mitosis/microservice-spring-kafka-reactor:1.0.0-alpha.0'
                 }                 
             """.stripIndent())
         }
--- generators/app/templates/ansible/images/dockerfiles/jenkins/dsl/spring5_gradle_pipelineJob.groovy
+++ generators/app/templates/ansible/images/dockerfiles/jenkins/dsl/spring5_gradle_pipelineJob.groovy
@@ -5,10 +5,17 @@ pipelineJob("spring5-gradle-pipeline") {
             script("""
                 node {
                       stage "checkout"
-
+                       git url : 'https://github.com/NirbyApp/mitosis-microservice-spring-reactor.git'
+                           
                       stage "test"
+                       sh './gradlew test'
+                      
+                      stage "build"
+                       sh './gradlew build'
 
                       stage "deploy"
+                       sh 'docker build -t mitosis/microservice-spring-reactor1.0.0-alpha.0 .'
+                       sh 'docker run -d -p 9991:8080 mitosis/microservice-spring-reactor:1.0.0-alpha.0'
                 }                 
             """.stripIndent())
         }
