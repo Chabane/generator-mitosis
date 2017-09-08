1d2db9231412c0fd615c74bf385140331ae473ed
Chabane <chabane.refes@gmail.com>
2017-09-08 23:06:22 +0200

    bugfix: fix path

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -1,6 +1,6 @@
-const ansible_swarm = require('ansible-swarm');
-const ansible_k8s = require('ansible-k8s');
-const ansible_images = require('ansible-images');
+const ansible_swarm = require('./ansible-swarm');
+const ansible_k8s = require('./ansible-k8s');
+const ansible_images = require('./ansible-images');
 const ANSIBLE_DIR = 'ansible';
 
 const expectedFiles = {
